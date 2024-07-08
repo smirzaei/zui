99cdb3603a053a0c68a4d7202b12af545273c384
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2024-07-07 21:41:26 -0400

    feat: pass string

--- app/main.zig
+++ app/main.zig
@@ -1,4 +1,10 @@
-pub fn main() !void {}
+const js = struct {
+    extern "js" fn log(arg: [*]const u8) void;
+};
+
+pub export fn greet() void {
+    js.log("Hello from Zig!");
+}
 
 pub export fn add(a: i32, b: i32) i32 {
     return a + b;
